cab0b1c2df02574eb66d420cde0c5cf9c0382052
vasug27 <vasugoel2754@gmail.com>
2025-09-27 01:51:01 +0530

    Added support for ffmpeg in api.py

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import os
 import json
+import tempfile
+import subprocess
 from flask import Flask, request, jsonify
 from vosk import Model, KaldiRecognizer
 from googletrans import Translator
@@ -16,6 +18,31 @@ if not os.path.exists(MODEL_DIR):
 model = Model(MODEL_DIR)
 
 
+def convert_to_wav(input_bytes):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".input") as input_file:
+        input_file.write(input_bytes)
+        input_path = input_file.name
+
+    output_path = input_path + ".wav"
+
+    cmd = [
+        "ffmpeg", "-y",
+        "-i", input_path,
+        "-ar", str(RATE),
+        "-ac", "1",
+        "-f", "s16le",
+        output_path
+    ]
+    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    with open(output_path, "rb") as f:
+        data = f.read()
+
+    os.remove(input_path)
+    os.remove(output_path)
+    return data
+
+
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Speech-to-Text API with Hindi Translation is running!"})
@@ -29,9 +56,11 @@ def recognize_audio():
     file = request.files["file"]
     audio_bytes = file.read()
 
+    wav_data = convert_to_wav(audio_bytes)
+
     recognizer = KaldiRecognizer(model, RATE)
 
-    if recognizer.AcceptWaveform(audio_bytes):
+    if recognizer.AcceptWaveform(wav_data):
         result = json.loads(recognizer.Result())
         text = result.get("text", "")
         hindi = translator.translate(text, src="en", dest="hi").text if text else ""
